3d4a576017a3a273ef5481ce78ae43f200412af7
Nisyhaal Tamil <nisyhaaltamil6@gmail.com>
2022-04-09 16:31:40 +0800

    Video recording feature addition

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from Functions import *
 
-# Loading the frontal face & eyes cascade classifiers obtained from https://github.com/opencv/opencv/tree/master/data
+# Loading the frontal face & eyes cascade classifiers stored in local obtained from https://github.com/opencv/opencv/tree/master/data
 faceCascadeClassifier = cascadeClassifierLoader('data/haarcascades/haarcascade_frontalface_alt.xml')
 eyesCascadeClassifier = cascadeClassifierLoader('data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
 
 stream = cv.VideoCapture(0)
 
+# Getting width & height of frame for video output configuration
+frameWidth = int(stream.get(3))
+frameHeight = int(stream.get(4))
+
+# Video output configuration
+videoOutput = cv.VideoWriter('output.avi', cv.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frameWidth, frameHeight))
+
 # Drawing a rectangular bounding box around detected faces and eyes
-while stream.isOpened():
+while True:
     # Read USB camera
     ret, frame = stream.read()
 
@@ -18,26 +25,29 @@ while stream.isOpened():
     faceDetection = faceCascadeClassifier.detectMultiScale(processedFrame)
 
     # Draw bounding boxes around faces
-    for (x, y, width, height) in faceDetection:
-        startPointBox = (x, y)
-        endPointBox = (x + width, y + height)
-        colorBox = (255, 0, 0)
-        thicknessBox = 2
-        frame = cv.rectangle(frame, startPointBox, endPointBox, colorBox, thicknessBox)
+    for (x_face, y_face, width_face, height_face) in faceDetection:
+        startPointBox_face = (x_face, y_face)
+        endPointBox_face = (x_face + width_face, y_face + height_face)
+        colorBox_face = (255, 0, 0)
+        thicknessBox_face = 2
+        frame = cv.rectangle(frame, startPointBox_face, endPointBox_face, colorBox_face, thicknessBox_face)
 
         # Apply Eye Cascade on processed frames, detect eyes and return the x-coordinate, y-coordinate, width & height of rectangles
         eyesDetection = eyesCascadeClassifier.detectMultiScale(processedFrame)
 
         # Draw bounding boxes around eyes
-        for (x2, y2, width2, height2) in eyesDetection:
-            startPointBox2 = (x2, y2)
-            endPointBox2 = (x2 + width2, y2 + height2)
-            colorBox = (0, 255, 0)
-            thicknessBox = 1
-            frame = cv.rectangle(frame, startPointBox2, endPointBox2, colorBox, thicknessBox)
+        for (x_eyes, y_eyes, width_eyes, height_eyes) in eyesDetection:
+            startPointBox_eyes = (x_eyes, y_eyes)
+            endPointBox_eyes = (x_eyes + width_eyes, y_eyes + height_eyes)
+            colorBox_eyes = (0, 255, 0)  # green
+            thicknessBox_eyes = 1
+            frame = cv.rectangle(frame, startPointBox_eyes, endPointBox_eyes, colorBox_eyes, thicknessBox_eyes)
 
     # Stream display
     cv.imshow('Stream - Face & Eye Detection using Haar Cascade', frame)
+
+    # Stream record
+    videoOutput.write(frame)
 
     # Break operation
     if cv.waitKey(10) == 27:  # Esc key
